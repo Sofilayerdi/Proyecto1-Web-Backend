939bf9d9e71b34d66b340fdc020ec90af2b2f395
Sofilayerdi <sofilayerdi@gmail.com>
2026-04-18 00:27:04 -0600

    Correcion rutas e inicializacion en main

--- main.go
+++ main.go
@@ -3,41 +3,40 @@ package main
 import (
 	"database/sql"
 	"log"
-	"net"
 	"net/http"
 
-	_ "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func router(conn net.Conn, db *sql.DB) {
+var db *sql.DB
 
-r:
-	chi.NewRouter()
-
-	r.Get("/series", listarSeries)
-	r.Get("/series/{id}", obtenerSerie)
-	r.Post("/series", crearSerie)
-	r.Put("/series/{id}", editarSerie)
-	r.Delete("/series/{id}", eliminarSerie)
-
-	http.ListenAndServe(conn, r)
+type Serie struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	CurrentEpisode int    `json:"current_ep"`
+	TotalEpisodes  int    `json:"total_ep"`
+	Img            string `json:"img"`
 }
 
 func main() {
-	listener, _ := net.Listen("tcp", ":8080")
-	defer listener.Close()
-
-	db, err := sql.Open("pgx", "postgres://postgres:TU_PASSWORD@localhost:5432/series_tracker?sslmode=disable")
+	//Inicializar database
+	var err error
+	db, err = sql.Open("pgx", "postgres://postgres:Duquesa0@localhost:5432/series?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	r := chi.NewRouter()
+
+	r.Get("/series", listarSeries)
+	r.Get("/series/{id}", verSerie)
+	r.Post("/series", crearSerie)
+	r.Put("/series/{id}", editarSerie)
+	r.Delete("/series/{id}", eliminarSerie)
+
 	log.Print("listening to port 8000")
 
-	for {
-		conn, _ := listener.Accept()
-		go router(conn, db)
-	}
+	http.ListenAndServe(":8000", r)
 }
